fddc934039949e40cc3007c1a065404d4b4256ac
Not-Naruto <farazriazchowdhury@gmail.com>
2023-08-24 19:45:54 +0300

    Added Analytics component

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ import BarChartIcon from "@mui/icons-material/BarChart";
 import LayersIcon from "@mui/icons-material/Layers";
 import { themeDark, themeLight } from "./theme";
 import BivariateComponent from "./components/BivariateComponent";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   const [colorMode, setColorMode] = useState<"light" | "dark">("dark");
@@ -31,86 +32,89 @@ function App() {
   const [screen, setScreen] = useState("fileInput");
 
   return (
-    <ThemeProvider theme={colorMode === "light" ? themeLight : themeDark}>
-      <CssBaseline />
-      <Backdrop
-        sx={{
-          color: "primary.main",
-          zIndex: (theme) => theme.zIndex.drawer + 1,
-        }}
-        open={loading}
-      >
-        <CircularProgress color="inherit" />
-      </Backdrop>
-      <NavBar
-        colorMode={colorMode}
-        toggleColorMode={() =>
-          setColorMode(colorMode === "dark" ? "light" : "dark")
-        }
-        toggleMenu={() => setMenuOpen(!menuOpen)}
-      />
-      <Aside_drawer
-        file={file}
-        menuOpen={menuOpen}
-        setMenuOpen={() => setMenuOpen(!menuOpen)}
-        setScreen={(scr: string) => setScreen(scr)}
-      />
-      <Grid container>
-        <Grid item id="aside" xs={0} sm={3.5} lg={2} xl={1.5}>
-          <Box
-            sx={{ backgroundColor: "primary.dark" }}
-            width={"100%"}
-            minHeight={"100vh"}
-            height={"100%"}
-          >
-            <List sx={{ display: { xs: "none", sm: "block" } }}>
-              <ListItem key="details" disablePadding>
-                <ListItemButton
-                  disabled={file ? false : true}
-                  onClick={() => setScreen("Details")}
-                >
-                  <ListItemIcon>
-                    <LayersIcon sx={{ color: "white" }} />
-                  </ListItemIcon>
-                  <ListItemText sx={{ color: "white" }} primary={"Details"} />
-                </ListItemButton>
-              </ListItem>
-              <ListItem key="bivariate" disablePadding>
-                <ListItemButton
-                  disabled={file ? false : true}
-                  onClick={() => setScreen("bivariate")}
-                >
-                  <ListItemIcon>
-                    <BarChartIcon sx={{ color: "white" }} />
-                  </ListItemIcon>
-                  <ListItemText
-                    sx={{ color: "white" }}
-                    primary={"Bivariate Graphs"}
-                  />
-                </ListItemButton>
-              </ListItem>
-            </List>
-          </Box>
+    <>
+      <ThemeProvider theme={colorMode === "light" ? themeLight : themeDark}>
+        <CssBaseline />
+        <Backdrop
+          sx={{
+            color: "primary.main",
+            zIndex: (theme) => theme.zIndex.drawer + 1,
+          }}
+          open={loading}
+        >
+          <CircularProgress color="inherit" />
+        </Backdrop>
+        <NavBar
+          colorMode={colorMode}
+          toggleColorMode={() =>
+            setColorMode(colorMode === "dark" ? "light" : "dark")
+          }
+          toggleMenu={() => setMenuOpen(!menuOpen)}
+        />
+        <Aside_drawer
+          file={file}
+          menuOpen={menuOpen}
+          setMenuOpen={() => setMenuOpen(!menuOpen)}
+          setScreen={(scr: string) => setScreen(scr)}
+        />
+        <Grid container>
+          <Grid item id="aside" xs={0} sm={3.5} lg={2} xl={1.5}>
+            <Box
+              sx={{ backgroundColor: "primary.dark" }}
+              width={"100%"}
+              minHeight={"100vh"}
+              height={"100%"}
+            >
+              <List sx={{ display: { xs: "none", sm: "block" } }}>
+                <ListItem key="details" disablePadding>
+                  <ListItemButton
+                    disabled={file ? false : true}
+                    onClick={() => setScreen("Details")}
+                  >
+                    <ListItemIcon>
+                      <LayersIcon sx={{ color: "white" }} />
+                    </ListItemIcon>
+                    <ListItemText sx={{ color: "white" }} primary={"Details"} />
+                  </ListItemButton>
+                </ListItem>
+                <ListItem key="bivariate" disablePadding>
+                  <ListItemButton
+                    disabled={file ? false : true}
+                    onClick={() => setScreen("bivariate")}
+                  >
+                    <ListItemIcon>
+                      <BarChartIcon sx={{ color: "white" }} />
+                    </ListItemIcon>
+                    <ListItemText
+                      sx={{ color: "white" }}
+                      primary={"Bivariate Graphs"}
+                    />
+                  </ListItemButton>
+                </ListItem>
+              </List>
+            </Box>
+          </Grid>
+          <Grid item id="main" xs={12} sm={8.5} lg={8} xl={9}>
+            {screen === "fileInput" ? (
+              <FileInput
+                setData={(res) => setData(res)}
+                setLoading={(res: boolean) => setLoading(res)}
+                setFile={(res: any) => setFile(res)}
+                setScreen={() => setScreen("Details")}
+              />
+            ) : screen === "Details" ? (
+              <Details data={data} file={file} />
+            ) : (
+              <BivariateComponent data={data} file={file} />
+            )}
+          </Grid>
+          <Grid item id="right" xs={12} sm={12} lg={2} xl={1.5}>
+            <Box width={"100%"} height={"100%"}></Box>
+          </Grid>
         </Grid>
-        <Grid item id="main" xs={12} sm={8.5} lg={8} xl={9}>
-          {screen === "fileInput" ? (
-            <FileInput
-              setData={(res) => setData(res)}
-              setLoading={(res: boolean) => setLoading(res)}
-              setFile={(res: any) => setFile(res)}
-              setScreen={() => setScreen("Details")}
-            />
-          ) : screen === "Details" ? (
-            <Details data={data} file={file} />
-          ) : (
-            <BivariateComponent data={data} file={file} />
-          )}
-        </Grid>
-        <Grid item id="right" xs={12} sm={12} lg={2} xl={1.5}>
-          <Box width={"100%"} height={"100%"}></Box>
-        </Grid>
-      </Grid>
-    </ThemeProvider>
+      </ThemeProvider>
+      <Analytics />
+    </>
   );
 }
 
